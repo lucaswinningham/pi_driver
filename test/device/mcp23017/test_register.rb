2b55312e0652b7fee58053e4d07134b62ec59897
Lucas Winningham <lucas.winningham@gmail.com>
2018-04-11 21:58:36 -0400

    add default register byte value tests

--- test/device/mcp23017/test_register.rb
+++ test/device/mcp23017/test_register.rb
@@ -61,4 +61,38 @@ class MCP23017RegisterTest < MCP23017Test
   #   assert_equal 0x19, @mcp23017.gpiob.address
   #   assert_equal 0x1a, @mcp23017.olatb.address
   # end
+
+  def test_byte_new
+    assert_equal 0b11111111, @mcp23017.iodira.byte
+    assert_equal 0b11111111, @mcp23017.iodirb.byte
+
+    assert_equal 0b00000000, @mcp23017.ipola.byte
+    assert_equal 0b00000000, @mcp23017.ipolb.byte
+
+    assert_equal 0b00000000, @mcp23017.gpintena.byte
+    assert_equal 0b00000000, @mcp23017.gpintenb.byte
+
+    assert_equal 0b00000000, @mcp23017.defvala.byte
+    assert_equal 0b00000000, @mcp23017.defvalb.byte
+
+    assert_equal 0b00000000, @mcp23017.intcona.byte
+    assert_equal 0b00000000, @mcp23017.intconb.byte
+
+    assert_equal 0b00000000, @mcp23017.iocon.byte
+
+    assert_equal 0b00000000, @mcp23017.gppua.byte
+    assert_equal 0b00000000, @mcp23017.gppub.byte
+
+    assert_equal 0b00000000, @mcp23017.intfa.byte
+    assert_equal 0b00000000, @mcp23017.intfb.byte
+
+    assert_equal 0b00000000, @mcp23017.intcapa.byte
+    assert_equal 0b00000000, @mcp23017.intcapb.byte
+
+    assert_equal 0b00000000, @mcp23017.gpioa.byte
+    assert_equal 0b00000000, @mcp23017.gpiob.byte
+
+    assert_equal 0b00000000, @mcp23017.olata.byte
+    assert_equal 0b00000000, @mcp23017.olatb.byte
+  end
 end
