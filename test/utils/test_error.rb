d62206cc81bc2040061ccadcdfd439e4a75f9d62
Lucas Winningham <lucas.winningham@gmail.com>
2018-04-13 15:05:57 -0400

    remove timeout seconds

--- test/utils/test_error.rb
+++ test/utils/test_error.rb
@@ -4,7 +4,7 @@ class UtilsErrorTest < UtilsTest
   def test_error_interrupt_new_block
     assert_raises ArgumentError do
       PiDriver::Utils::Interrupt.new(:rising)
-      timeout 0.1 { false }
+      timeout { false }
     end
   end
 
@@ -12,7 +12,7 @@ class UtilsErrorTest < UtilsTest
     assert_raises ArgumentError do
       interrupt = PiDriver::Utils::Interrupt.new(:rising) { [0, 1].sample }
       interrupt.start
-      timeout 0.1 { false }
+      timeout { false }
     end
   end
 
