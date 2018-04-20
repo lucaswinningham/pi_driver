e341ffcff390fc44cbb6e4fd3e5c59a4749f55b2
Lucas Winningham <lucas.winningham@gmail.com>
2018-04-19 21:06:21 -0400

    change pin numbers to new pinout

--- test/integration/pin_test_helper.rb
+++ test/integration/pin_test_helper.rb
@@ -1,9 +1,9 @@
 require_relative '../integration_test_helper'
 
-# for all integration pin tests, connect pins 11 and 12 together
+# for all integration pin tests, connect pins 3 and 5 together
 class IntegrationPinTest < IntegrationTest
   def setup
-    @setter = PiDriver::Pin.new 0, direction: :out
-    @getter = PiDriver::Pin.new 1
+    @setter = PiDriver::Pin.new 2, direction: :out
+    @getter = PiDriver::Pin.new 3
   end
 end
