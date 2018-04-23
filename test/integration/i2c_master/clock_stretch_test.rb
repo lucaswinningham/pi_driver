4f957e53cd105e6ba6a63ef8f4508ef70ffaa553
Lucas Winningham <lucas.winningham@gmail.com>
2018-04-23 18:48:46 -0400

    add empty clock stretch and speed tests for i2c integration

---
+++ test/integration/i2c_master/clock_stretch_test.rb
@@ -0,0 +1,9 @@
+require_relative '../i2c_master_test_helper'
+
+class IntegrationI2CClockStretchTest < IntegrationI2CMasterTest
+  # TODO: implement
+  def test_clock_stretch; end
+
+  # TODO: implement
+  def test_clock_stretch_timeout; end
+end
---
+++ test/integration/i2c_master/speed_test.rb
@@ -0,0 +1,6 @@
+require_relative '../i2c_master_test_helper'
+
+class IntegrationI2CSpeedTest < IntegrationI2CMasterTest
+  # TODO: implement
+  def test_speed; end
+end
