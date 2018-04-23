719128944bf94abfd4c01a754ce932200e2f0343
Lucas Winningham <lucas.winningham@gmail.com>
2018-04-23 13:16:22 -0400

    add i2c start and stop integration tests

---
+++ test/integration/i2c_master/start_test.rb
@@ -0,0 +1,23 @@
+require_relative '../i2c_master_test_helper'
+
+class IntegrationI2CStartTest < IntegrationI2CMasterTest
+  def test_start
+    data_pin_driven = false
+    clock_pin_driven = false
+    data_pin_driven_first = false
+
+    @slave_sda.interrupt(:falling) do |edge|
+      data_pin_driven = true
+      data_pin_driven_first = !clock_pin_driven
+    end
+
+    @slave_scl.interrupt(:falling) { |edge| clock_pin_driven = true }
+
+    @i2c_master.start
+    timeout { data_pin_driven && clock_pin_driven && data_pin_driven_first }
+    assert data_pin_driven && clock_pin_driven && data_pin_driven_first
+
+    @slave_sda.clear_interrupt
+    @slave_scl.clear_interrupt
+  end
+end
---
+++ test/integration/i2c_master/stop_test.rb
@@ -0,0 +1,23 @@
+require_relative '../i2c_master_test_helper'
+
+class IntegrationI2CStopTest < IntegrationI2CMasterTest
+  def test_stop
+    data_pin_released = false
+    clock_pin_released = false
+    clock_pin_released_first = false
+
+    @slave_scl.interrupt(:rising) do |edge|
+      clock_pin_released = true
+      clock_pin_released_first = !data_pin_released
+    end
+
+    @slave_sda.interrupt(:rising) { |edge| data_pin_released = true }
+
+    @i2c_master.stop
+    timeout { clock_pin_released && data_pin_released && clock_pin_released_first }
+    assert clock_pin_released && data_pin_released && clock_pin_released_first
+
+    @slave_scl.clear_interrupt
+    @slave_sda.clear_interrupt
+  end
+end
