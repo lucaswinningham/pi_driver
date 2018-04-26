44e8062b199ddc992aa6fa378d6c8bc21e067ee0
Lucas Winningham <lucas.winningham@gmail.com>
2018-04-26 16:45:24 -0400

    add to bash

--- bin/.pi_commands.sh
+++ bin/.pi_commands.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 function pi() {
+  working_directory=$(basename "$PWD")
+  print_help=false
   if [ "$1" == install ]; then
     echo -e "not implemented yet... coming soon."
     # cd
@@ -23,9 +25,35 @@ function pi() {
     # bundle
 
     # echo -e "\n\n\tSuccessfully installed Pi Driver.\n\n"
-  elif [ "$1" == console ]; then
-    irb -r ./lib/pi_driver
-  elif [ "$1" == test ]; then
-    rake test "${@:2}"
+  elif [ "$working_directory" == "pi_driver" ]; then
+    if [ "$1" == console ]; then
+      irb -r ./lib/pi_driver
+    elif [ "$1" == test ]; then
+      rake test "${@:2}"
+    elif [ "$1" == help ]; then
+      print_help=true
+    else
+      print_help=true
+    fi
+  else
+    print_help=true
+  fi
+
+  if [ "$print_help" == true ]; then
+    read -r -d '' help_message << HELP
+Usage:
+  pi [options]
+
+Options:
+  install             # Automatically install and update software and packages
+  console             # Open irb enviroment for use with Pi Driver
+  help                # Show this help message and quit
+  test [test_options] # Test the Pi Driver
+
+Test Options:
+  classes [directory] [test_prefix]     # Test the program suite
+  integration [directory] [test_prefix] # Test real circuitry
+HELP
+    echo -e "$help_message"
   fi
 }
