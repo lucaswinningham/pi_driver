1f113b9e2668b99a2c7d6345732f54748baf0a04
Lucas Winningham <lucas.winningham@gmail.com>
2018-05-04 12:03:47 -0400

    ruboocop

--- test/classes/pin/is_clear_test.rb
+++ test/classes/pin/is_clear_test.rb
@@ -9,7 +9,7 @@ class PinIsClearTest < PinTest
 
   def test_is_clear_output
     pin = PiDriver::Pin.new @gpio_number, direction: :out
-    expect_value_read'0'
+    expect_value_read '0'
     assert pin.clear?
   end
 
--- test/classes/pin/is_set_test.rb
+++ test/classes/pin/is_set_test.rb
@@ -15,13 +15,13 @@ class PinIsSetTest < PinTest
 
   def test_is_not_set_input
     pin = PiDriver::Pin.new @gpio_number, direction: :in
-    expect_value_read'0'
+    expect_value_read '0'
     refute pin.set?
   end
 
   def test_is_not_set_output
     pin = PiDriver::Pin.new @gpio_number, direction: :out
-    expect_value_read'0'
+    expect_value_read '0'
     refute pin.set?
   end
 end
