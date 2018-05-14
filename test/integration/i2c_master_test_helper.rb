18252b1cc6380f566595b5f6ef52a4853cd9c999
Lucas Winningham <lucas.winningham@gmail.com>
2018-05-14 08:42:16 -0400

    reset i2c master test integration pins to input

--- test/integration/i2c_master_test_helper.rb
+++ test/integration/i2c_master_test_helper.rb
@@ -3,10 +3,10 @@ require_relative '../integration_test_helper'
 class IntegrationI2CMasterTest < IntegrationTest
   def setup
     super
-    @master_scl = PiDriver::Pin.new 12
-    @slave_scl = PiDriver::Pin.new 16
-    @master_sda = PiDriver::Pin.new 20
-    @slave_sda = PiDriver::Pin.new 21
+    @master_scl = PiDriver::Pin.new 12, direction: :in
+    @slave_scl = PiDriver::Pin.new 16, direction: :in
+    @master_sda = PiDriver::Pin.new 20, direction: :in
+    @slave_sda = PiDriver::Pin.new 21, direction: :in
 
     @i2c_master = PiDriver::I2CMaster.new clock_pin: @master_scl, data_pin: @master_sda
 
